7fdf584bae6dac61b96d9a4a9138a04d537bfc51
Will Pak Poy (Ubuntu) <willmail@internode.on.net>
2020-10-28 22:15:20 +1100

    second demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hero demo">
+    <p>Over a (number)<br/>of sharks are killed<br/>every year.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped lang="scss">
+.demo {
+  height: 100vh;
+  background: url(https://images.pexels.com/photos/1700656/pexels-photo-1700656.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940) no-repeat center center fixed;
+  background-size: cover;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  p {
+    margin: 0px;
+    padding: 64px;
+    font-size: 90px;
+    color: #fff;
+    text-align: right;
+    font-family: acumin-pro-condensed;
+    text-transform: uppercase;
+  }
+}
+</style>
