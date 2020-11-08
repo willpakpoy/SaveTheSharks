aff017ba330aebca8391aef1e6954606151ae93b
Will Pak Poy (Mac) <0521858@radford.act.edu.au>
2020-11-08 12:20:22 +1100

    Add Gtag

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import VueScrollTo from "vue-scrollto";
+import VueGtag from "vue-gtag-next";
 
 createApp(App)
   .use(store)
   .use(router)
   .use(VueScrollTo)
+  .use(VueGtag, {
+    property: {
+      id: "G-6CGFWR0Y8M"
+    }
+  })
   .mount("#app");
